b60dd42b55dd4915ff98cda8109807d9bc191271
jaubourg <aubourg.julian@gmail.com>
2009-12-22 02:00:48 +0100

    Renamed testParse to parse and added a parse data serializer.
    Signed-off-by: jaubourg <aubourg.julian@gmail.com>

--- src/test.js
+++ src/test.js
@@ -1,5 +1,25 @@
 // Enables to inspect parser's outputs
-dominoes.testParse = function() {
+dominoes.parse = function() {
 	return parseList( arguments );
 };
+
+// View parse result
+dominoes.viewParse = function( parse ) {
+	
+	var output = "",
+		cumul = [];
+	
+	if ( isArray( parse ) ) {
+		for ( var i in parse ) {
+			cumul.push( dominoes.viewParse( parse[i] ) );
+		}
+		output = "[ "+cumul.join(" , ")+" ]";
+	} else if ( isFunction( parse ) ) {
+		output = "<Function>";
+	} else {
+		output = "" + parse;
+	}
+	
+	return output;
+}
 
