c9cb6b1c953ba694294e3a3ea00681cb836da314
jaubourg <aubourg.julian@gmail.com>
2009-12-23 00:03:13 +0100

    Added the possibility to erase a rule
    Signed-off-by: jaubourg <aubourg.julian@gmail.com>

--- src/rule.js
+++ src/rule.js
@@ -2,56 +2,69 @@ var	rules = {},
 	rulesInternals = {};
 
 // Declare or get a rule
-dominoes.rule = function( id ) {
+dominoes.rule = function( id , del ) {
 	
 	var length = arguments.length;
 	
 	if ( length > 1 ) {
 		
-		var list = parseList( slice.call( arguments , 1 ) ),
-			ruleInternal = rulesInternals[ id ];
+		if ( del === false ) {
 			
-		// Create entry no matter what
-		if ( ! ruleInternal ) {
-
-			var go = function() {
-					execute( ruleInternal , function() {
-						if ( running = list.length ) {
-							running--;
-							list.shift()();
-							if ( running ) {
-								go();
-							}
-						}
-					} );
-				},
-				running;
+			try {
 				
-			ruleInternal = rulesInternals[ id ] = [];
-			rules[ id ] = function ( callback ) {
-				if ( isFunction(callback) ) {
-					list.push( callback );
-				}
-				if ( ! running ) {
-					running = TRUE;
-					go();
-				}
-				return true;
-			};
-		}
-		
-		// Filter out empty lists
-		if ( list.length ) {
-	
-			// Note as non optional
-			list.push( FALSE );
+				delete rules[ id ];
+				delete rulesInternals[ id ];
 			
-			// Add in
-			ruleInternal.push( list );
-		}
+			} catch( _ ) {}
+			
+		} else {
 		
-		// Free list for re-use
-		list = [];
+			var list = parseList( slice.call( arguments , 1 ) ),
+				ruleInternal = rulesInternals[ id ];
+				
+			// Create entry no matter what
+			if ( ! ruleInternal ) {
+	
+				var go = function() {
+						execute( ruleInternal , function() {
+							if ( running = list.length ) {
+								running--;
+								list.shift()();
+								if ( running ) {
+									go();
+								}
+							}
+						} );
+					},
+					running;
+					
+				ruleInternal = rulesInternals[ id ] = [];
+				rules[ id ] = function ( callback ) {
+					if ( isFunction(callback) ) {
+						list.push( callback );
+					}
+					if ( ! running ) {
+						running = TRUE;
+						go();
+					}
+					return true;
+				};
+			}
+			
+			// Filter out empty lists
+			if ( list.length ) {
+		
+				// Note as non optional
+				list.push( FALSE );
+				
+				// Add in
+				ruleInternal.push( list );
+			}
+			
+			// Free list for re-use
+			list = [];
+			
+		}
 		
 	} else if ( length ) {
 		
