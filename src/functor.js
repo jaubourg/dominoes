7b4ea6be2f18d504f547e2f62af51f8e489c596f
jaubourg <aubourg.julian@gmail.com>
2010-01-09 05:58:38 +0100

    Fixed typo (; instead of ,)
    Signed-off-by: jaubourg <aubourg.julian@gmail.com>

--- src/functor.js
+++ src/functor.js
@@ -46,7 +46,7 @@ var functor = dominoes.functor = dataHolder( function( id , func ) {
 					
 				},
 				types = functor.T = functor.T || {},
-				typesString = parts[ 2 ] ? parts[ 2 ].replace( /^{\s*|\s*}$/g , "" ) : "";
+				typesString = parts[ 2 ] ? parts[ 2 ].replace( /^{\s*|\s*}$/g , "" ) : "",
 				tmp = ( typesString || "F|S|O" ).split( /\s*\|\s*/ ),
 				length = tmp.length,
 				i = 0;
