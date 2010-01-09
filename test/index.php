db20f6993306a6a0db6d6dea9b85370c904ccacd
jaubourg <aubourg.julian@gmail.com>
2010-01-09 02:59:44 +0100

    Use full version instead of minified version for tests
    Signed-off-by: jaubourg <aubourg.julian@gmail.com>

--- test/index.php
+++ test/index.php
@@ -16,7 +16,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 	<!-- Some utilities -->
 	<script type="text/javascript" src="./utils.js"></script>
 	<!-- Dominoes -->
-	<script type="text/javascript" src="../dist/dominoes.min.js"></script>
+	<script type="text/javascript" src="../dist/dominoes.js"></script>
 	<!-- Tests suites -->
 <?php
     $list = array();
