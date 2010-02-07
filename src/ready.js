48185d98ba5d9f213d158a2bc643ec4a81775b8d
jaubourg <aubourg.julian@gmail.com>
2010-02-07 13:41:35 +0100

    Re-organised ready code and removed non-working IE code (soooo tired!). For now, the IE DOM readyness code is a window.onload... :/
    Signed-off-by: jaubourg <aubourg.julian@gmail.com>

--- src/ready.js
+++ src/ready.js
@@ -1,90 +1,66 @@
-var readyCallbacks = [],
-	readyIndex,
-	readyListenedTo = FALSE,
-	readyAcknowledged = FALSE,
-	readyFireing = FALSE,
-	readyLink;
+var ready = ( function ( readyCallbacks , readyListenedTo , readyAcknowledged , readyFireing ) {
 	
-function fireReady() {
-	
-	for ( readyIndex = 0 ; readyIndex < readyCallbacks[ STR_LENGTH ] ; ) {
-			args = readyCallbacks[ readyIndex++ ];
-			args[ 0 ][ STR_APPLY ]( document , slice[ STR_CALL ]( args , 1 ) );
-	}
-	
-	readyCallbacks = [];
-	
-	readyFireing = FALSE;
-	
-}
-
-function acknowledgeReady() {
-	
-	if ( ! readyAcknowledged ) {
-
-		readyAcknowledged = TRUE;
-		later( ready );
+	function fireReady( index , args ) {
+		
+		for ( index = 0 ; index < readyCallbacks[ STR_LENGTH ] ; ) {
+				args = readyCallbacks[ index++ ];
+				args[ 0 ][ STR_APPLY ]( args[ 1 ] , args[ 2 ] );
+		}
+		
+		readyCallbacks = [];
+		
+		readyFireing = FALSE;
 		
 	}
-}
-
-function ready( func ) {
 	
-	if ( isFunction ( func ) ) {
+	function acknowledgeReady() {
 		
-		readyCallbacks[ STR_PUSH ]( arguments );
-		
-	}
-		
-	if ( ! readyListenedTo ) {
-		
-		readyListenedTo = TRUE;
-		
-		readyAcknowledged = loadedCompleteRegExp.test( document[ STR_READY_STATE ] );
-			
 		if ( ! readyAcknowledged ) {
-
-			if ( document[ STR_ADD_EVENT_LISTENER ] ) {
-
-				document[ STR_ADD_EVENT_LISTENER ]( "DOMContentLoaded" , acknowledgeReady , FALSE );
-				window[ STR_ADD_EVENT_LISTENER ]( "load" , acknowledgeReady , FALSE );
+	
+			readyAcknowledged = readyFireing = TRUE;
+			fireReady();
+			
+		}
+	}
+	
+	return dominoes.ready = function ( func , context ) {
+		
+		if ( isFunction ( func ) ) {
+			
+			readyCallbacks[ STR_PUSH ]( [ func , context || document , slice[ STR_CALL ]( arguments , 2 ) ] );
+			
+			if ( ! readyListenedTo ) {
 				
-			} else if ( window[ STR_ATTACH_EVENT ] ) {
+				readyListenedTo = TRUE;
 				
-				window[ STR_ATTACH_EVENT ]( STR_ON_LOAD , acknowledgeReady );
-				
-				readyLink = document[ STR_CREATE_ELEMENT ]( "link" );
-				
-				readyLink[ STR_HREF ] = document.location;
-				readyLink.rel = STR_STYLESHEET;
-				readyLink[ STR_MEDIA ] = STR_PLUS;
-				
-				readyLink[ STR_ON_READY_STATE_CHANGE ] = function() {
+				readyAcknowledged = loadedCompleteRegExp.test( document[ STR_READY_STATE ] );
 					
-					if ( loadedCompleteRegExp.test( readyLink[ STR_READY_STATE ] ) ) {
+				if ( ! readyAcknowledged ) {
+					
+					if ( document[ STR_ADD_EVENT_LISTENER ] ) {
+		
+						document[ STR_ADD_EVENT_LISTENER ]( "DOMContentLoaded" , acknowledgeReady , FALSE );
+						window[ STR_ADD_EVENT_LISTENER ]( "load" , acknowledgeReady , FALSE );
 						
-						head[ STR_REMOVE_CHILD ]( readyLink );
-						acknowledgeReady();
+					} else if ( window[ STR_ATTACH_EVENT ] ) {
+						
+						window[ STR_ATTACH_EVENT ]( STR_ON_LOAD , acknowledgeReady );
 						
 					}
-					
 				}
 				
-				head[ STR_APPEND_CHILD ]( readyLink );
+			}
+			
+			if ( readyAcknowledged && ! readyFireing ) {
+				
+				readyFireing = TRUE;
+				fireReady();
+				
 			}
 		}
 		
-	}
+		return FALSE;
+	};
 	
-	if ( readyAcknowledged && ! readyFireing ) {
-		
-		readyFireing = TRUE;
-		fireReady();
-		
-	}
-	
-	return FALSE;
-}
-
-dominoes.ready = ready;
+} )( [] );
 
